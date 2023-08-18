d4a53a30b83088879d9fbc5f2ab09e492b3cce0e
hanab11 <sxakln2020@163.com>
2023-08-18 23:05:56 +0800

    按需引入ui库#1.0

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  // 按需引入mint-ui
+  plugins: [
+    ["component",
+      {
+        "libraryName": "mint-ui",
+        "style": true
+      }
+    ]
   ]
 }
