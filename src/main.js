46b058364027dabcf19abc3e3cbeef12b69c7cb0
hanab11 <sxakln2020@163.com>
2023-08-19 21:36:12 +0800

    main#1.0

--- src/main.js
+++ src/main.js
@@ -1,24 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue' //引入App组件
-import { Button } from 'mint-ui'
-import VueLazyload from 'vue-lazyload'
-import router from './router' //引入路由器
-import store from './store' //引入vuex仓库
+import router from './router' // 引入路由器
+import store from './store' // 引入vuex仓库
+import { Button } from 'mint-ui' // 引入mint-ui
+import VueLazyload from 'vue-lazyload' // 引入lazyload
+import App from './App.vue' // 引入App组件
 
-import './mock/mockServer' //加载mockServer即可
-import loading from './assets/images/loading.gif'
+import './mock/mockServer' // 加载mockServer即可
+import loading from './assets/images/loading.gif' // 引入加载图
 
-//关闭Vue的生产提示
-Vue.config.productionTip = false
-//注册全局组件标签
-Vue.component(Button.name, Button)  //<mt-button>
-Vue.use(VueLazyload, { //内部自定义一个指令lazy
-  loading
-})
+Vue.config.productionTip = false // 关闭Vue的生产提示
 
+Vue.component(Button.name, Button) // 注册全局组件标签<mt-button>
+
+Vue.use(VueLazyload, { loading }) // 使用插件并在vue内部自定义指令（v-lazy）
 
 new Vue({
-  render: h => h(App), //将App组件放入容器
-  router, //配置项vue-router
-  store, //配置项vuex的store
+  render: h => h(App), // 将App组件放入容器
+  router, // 配置项vue-router
+  store, // 配置项vuex的store
 }).$mount('#app')
