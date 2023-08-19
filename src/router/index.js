475deff3d37ead180eecf61d956c4720b3292401
hanab11 <sxakln2020@163.com>
2023-08-19 21:38:03 +0800

    按需加载路由，编程式路由重复点击导航问题#1.0

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,31 @@
-/* 
-该文件专门用于创建整个应用的路由器
+/**
+ * 路由器
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
-/* 
-import MSite from '../pages/MSite.vue'
-import Search from '../pages/Search.vue'
-import Order from '../pages/Order.vue'
-import Profile from '../pages/Profile.vue'
- */
 import Login from '../pages/Login.vue'
 import Shop from '../pages/Shop.vue'
 import ShopGoods from '../pages/ShopGoods.vue'
 import ShopRatings from '../pages/ShopRatings.vue'
 import ShopInfo from '../pages/ShopInfo.vue'
 
-// 返回路由组件的函数，只有执行此函数才会加载路由组件（按需），这个函数在请求对应的路由路径时才会执行
+// 用import函数返回路由组件，执行此函数加载路由组件（按需），首次请求对应的路由路径时才会执行此函数
 const MSite = () => import('../pages/MSite.vue')
 const Search = () => import('../pages/Search.vue')
 const Order = () => import('../pages/Order.vue')
 const Profile = () => import('../pages/Profile.vue')
 
+Vue.use(VueRouter) // 使用插件
 
-// 使用插件
-Vue.use(VueRouter)
-
-/* 
-  ！解决编程式路由重复点击导航引发的错误
-    先保存VueRouter原型对象的push、replace, 再重写push、replace
-  call、apply区别
-    相同点，都可以调用函数一次，都可以篡改函数的上下文一次
-    不同点：call与apply传递参数：call传递参数用逗号隔开，apply方法执行。传递数组
-    this还是用该上下文调用，也就是vuerouter的实例，后面跟参数location指定跳的位置，以及成功失败的回调
+/**
+ * 解决编程式路由重复点击导航引发的错误！
+ * 先保存VueRouter原型对象的push、replace，再重写push、replace
+ * 
+ * call、apply区别
+ * 相同点：都可以调用函数一次，都可以篡改函数的上下文一次
+ * 不同点：call与apply传递参数方式，call传递参数用逗号隔开，apply方法执行。传递数组
+ * 
+ * this还是该上下文调用，也就是vuerouter的实例，后面跟参数location指定跳的位置，以及成功失败的回调
  */
 let originPush = VueRouter.prototype.push
 let originReplace = VueRouter.prototype.replace
@@ -53,7 +46,6 @@ VueRouter.prototype.replace = function (location, resolve, reject) {
 }
 
 export default new VueRouter({
-  // 所有路由
   routes: [
     {
       path: '/msite',
