435dea9baf3204af81b2ca7fc347e35d340d8915
hanab11 <sxakln2020@163.com>
2023-08-19 21:39:05 +0800

    ajax请求函数、接口请求函数模块#1.0

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,42 +1,41 @@
-/*
-ajax请求函数模块
-返回值: promise对象(异步返回的数据是: response.data)
+/**
+ * ajax请求函数模块
+ * 返回值：promise对象，data也是promise对象
  */
 import axios from 'axios'
-export default function ajax(url, data = {}, type = 'GET') {
 
+export default function ajax(url, data = {}, type = 'GET') {
+  // 用高阶函数封装，返回值是resolve或reject的结果
   return new Promise(function (resolve, reject) {
-    // 执行异步ajax请求
-    let promise
+    // 一、执行异步ajax请求（用axios）
+    let promise // 准备promise对象
     if (type === 'GET') {
-      // 准备url query参数数据
-      let dataStr = '' //数据拼接字符串
+      let dataStr = '' // 准备拼接字符串（按query参数格式）
+
       Object.keys(data).forEach(key => {
-        dataStr += key + '=' + data[key] + '&'
+        dataStr += key + '=' + data[key] + '&' // 最后&结尾
       })
       if (dataStr !== '') {
         dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
         url = url + '?' + dataStr
       }
-      // 发送get请求
-      promise = axios.get(url)
+      promise = axios.get(url) // 发送get请求（带参url）
+
     } else {
-      // 发送post请求
-      promise = axios.post(url, data)
+      promise = axios.post(url, data) // 发送post请求（原url和数据）
     }
-    promise.then(function (response) {
-      // 成功了调用resolve()
-      resolve(response.data)
-    }).catch(function (error) {
-      //失败了调用reject()
-      reject(error)
-    })
+
+    // 二、等待异步response响应（用promise的then、catch，也可以用async、await实现）
+    promise.then(
+      (response) => resolve(response.data) // 成功了调用resolve()返回data
+    ).catch(
+      (error) => reject(error) // 失败了调用reject()返回error
+    )
   })
 }
-
-/*
-const response = await ajax()
+/* 
+const response = await ajax() // 不能直接取data是因为结果不确定，可能是response、error对象
 const result = response.data
 
-const resule = await ajax()
+const result = await ajax() // 封装的目的，直接获取结果而不是响应对象
  */
