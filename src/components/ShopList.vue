a83a90b2b0e6ffc1b7e54b80759231bd2343b5ff
hanab11 <sxakln2020@163.com>
2023-08-20 21:19:50 +0800

    商家列表的图片接口问题#1.0

--- src/components/ShopList.vue
+++ src/components/ShopList.vue
@@ -1,58 +1,57 @@
 <template>
   <div class="shop_container">
+    <!-- 商家列表 -->
     <ul class="shop_list" v-if="shops.length">
-      <li
-        class="shop_li border-1px"
-        v-for="(shop, index) in shops"
-        :key="index"
-        @click="$router.push('/shop')"
-      >
+      <!-- 如果想要点击跳转对应路由，依赖每个shop唯一标识id -->
+      <li class="shop_li border-1px" v-for="(shop, index) in shops" :key="index" @click="goToShop(shop.id)">
         <a>
+          <!-- 自建图床、上传图片、拆分url，在后端接口里修改 -->
           <div class="shop_left">
             <img class="shop_img" :src="baseImgUrl + shop.image_path" />
           </div>
           <div class="shop_right">
             <section class="shop_detail_header">
               <h4 class="shop_title ellipsis">{{ shop.name }}</h4>
+              <!-- 遍历活动小图标 -->
               <ul class="shop_detail_ul">
-                <li
-                  class="supports"
-                  v-for="(support, index) in shop.supports"
-                  :key="index"
-                >
+                <li class="supports" v-for="(support, index) in shop.supports" :key="index">
                   {{ support.icon_name }}
                 </li>
               </ul>
             </section>
             <section class="shop_rating_order">
               <section class="shop_rating_order_left">
-                <Star :score="shop.rating" :size="24" />
                 <div class="rating_section">
-                  {{ shop.rating }}
+                  {{ shop.rating }}<span>&nbsp;</span>
                 </div>
+                <!-- 星星组件 -->
+                <Star :score="shop.rating" :size="24" />
                 <div class="order_section">
                   月售{{ shop.recent_order_num }}单
                 </div>
               </section>
               <section class="shop_rating_order_right">
-                <span class="delivery_style delivery_right">{{
-                  shop.delivery_mode.text
-                }}</span>
+                <span class="delivery_style delivery_right">
+                  {{ shop.delivery_mode.text }}
+                </span>
               </section>
             </section>
             <section class="shop_distance">
               <p class="shop_delivery_msg">
-                <span>¥{{ shop.float_minimum_order_amount }}起送</span>
-                <span class="segmentation">/</span>
-                <span>配送费约¥{{ shop.float_delivery_fee }}</span>
+                <span>起送¥{{ shop.float_minimum_order_amount }}</span>
+                <span>&nbsp;</span>
+                <span v-if="shop.float_delivery_fee">配送费约¥{{ shop.float_delivery_fee }}</span>
+                <span class="delivery_free" v-else>免配送费</span>
               </p>
             </section>
           </div>
         </a>
       </li>
+      <!-- 防遮挡占位使用css的fixed吸顶、吸底来处理 -->
     </ul>
+    <!-- 原型占位 -->
     <ul v-else>
-      <li v-for="item in 6">
+      <li v-for="item in 8">
         <img src="../assets/images/shop_back.svg" alt="back" />
       </li>
     </ul>
@@ -64,16 +63,20 @@ import { mapState } from 'vuex'
 import Star from './Star.vue'
 
 export default {
+  components: { Star },
   data() {
     return {
-      baseImgUrl: 'http://cangdu.org:8001/img/',
+      baseImgUrl: 'https://i.postimg.cc/',
     }
   },
   computed: {
     ...mapState(['shops']),
   },
-  components: {
-    Star,
+  methods: {
+    goToShop(id) {
+      this.$store.dispatch('recordShopId', id) // 同步保存id到vuex的state
+      this.$router.push('/shop') // 编程式路由push
+    },
   },
 }
 </script>
@@ -196,9 +199,12 @@ export default {
               transform-origin: 0;
               transform: scale(0.9);
               color: #666;
-
               .segmentation {
                 color: #ccc;
+              }
+              .delivery_free {
+                color: #ff8000;
+                font-weight: bold;
               }
             }
           }
