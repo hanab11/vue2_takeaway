71e10441882fb24aef76c23805e16340d312500f
hanab11 <sxakln2020@163.com>
2023-08-20 21:17:58 +0800

    购物车组件只有一个列表，动态的加减数据，创建单例滑动，刷新滑动条（不能和多列表，创建滑动混淆）#1.0

--- src/components/ShopCart.vue
+++ src/components/ShopCart.vue
@@ -2,25 +2,20 @@
   <div>
     <div class="shopcart">
       <div class="content">
+        <!-- 购物车外层 -->
         <div class="content-left" @click="toggleShow">
           <div class="logo-wrapper">
             <div class="logo" :class="{ highlight: totalCount }">
-              <i
-                class="iconfont icon-shopping_cart"
-                :class="{ highlight: totalCount }"
-              ></i>
+              <i class="iconfont icon-shopping_cart" :class="{ highlight: totalCount }"></i>
             </div>
             <div class="num" v-if="totalCount">{{ totalCount }}</div>
           </div>
-          <div class="price" :class="{ highlight: totalCount }">
-            ￥{{ totalPrice }}
-          </div>
+          <div class="price" :class="{ highlight: totalCount }">￥{{ totalPrice }}</div>
           <div class="desc">另需配送费￥{{ info.deliveryPrice }}元</div>
         </div>
+        <!-- 支付按钮样式、支付内容 -->
         <div class="content-right">
-          <div class="pay" :class="payClass">
-            {{ payText }}
-          </div>
+          <div class="pay" :class="payClass">{{ payText }}</div>
         </div>
       </div>
       <transition name="move">
@@ -29,6 +24,7 @@
             <h1 class="title">购物车</h1>
             <span class="empty" @click="clearCart">清空</span>
           </div>
+          <!-- 购物车列表 -->
           <div class="list-content">
             <ul>
               <li class="food" v-for="(food, index) in cartFoods" :key="index">
@@ -36,6 +32,7 @@
                 <div class="price">
                   <span>￥{{ food.price }}</span>
                 </div>
+                <!-- 对列表每个元素配上购物车控件 -->
                 <div class="cartcontrol-wrapper">
                   <CartControl :food="food" />
                 </div>
@@ -45,6 +42,7 @@
         </div>
       </transition>
     </div>
+    <!-- 列表遮罩 -->
     <div class="list-mask" v-show="listShow" @click="toggleShow"></div>
   </div>
 </template>
@@ -56,76 +54,85 @@ import { mapState, mapGetters } from 'vuex'
 import CartControl from './CartControl.vue'
 
 export default {
+  components: { CartControl },
   data() {
     return {
-      isShow: false,
+      isShow: false, // 是否展示，用于计算列表是否展示
     }
   },
-
   computed: {
     ...mapState(['cartFoods', 'info']),
     ...mapGetters(['totalCount', 'totalPrice']),
+
     payClass() {
       const { totalPrice } = this
-      const { minPrice } = this.info
+      const { minPrice } = this.info // 准备数据
 
       return totalPrice >= minPrice ? 'enough' : 'not-enough'
     },
     payText() {
       const { totalPrice } = this
-      const { minPrice } = this.info
-      if (totalPrice === 0) {
-        return `￥${minPrice}元起送`
-      } else if (totalPrice < minPrice) {
+      const { minPrice } = this.info // 准备数据
+
+      if (totalPrice === 0) return `￥${minPrice}元起送`
+      else if (totalPrice < minPrice)
         return `还差￥${minPrice - totalPrice}元起送`
-      } else {
-        return '结算'
-      }
+      else return '结算'
     },
-
-    listShow() {
-      // 如果总数量为0, 直接不显示
-      if (this.totalCount === 0) {
-        this.isShow = false
-        return false
-      }
-
-      if (this.isShow) {
-        this.$nextTick(() => {
-          // 实现BScroll的实例是一个单例
-          if (!this.scroll) {
-            this.scroll = new BScroll('.list-content', {
-              click: true,
-            })
-          } else {
-            this.scroll.refresh() // 让滚动条刷新一下: 重新统计内容的高度
-          }
-        })
-      }
-
-      return this.isShow
+    // 计算属性的简写一般是只读，想要写得用完整写法，注意读写是分开的
+    listShow: {
+      /**
+       * 购物车显示隐藏
+       * 根据toggleShow（isShow会被修改）
+       * 还有totalCount减为0的特殊情况（isShow未更改，还是true）
+       * 一个属性随着另一个属性，要及时变化，要想到监视属性
+       */
+      // 读计算属性
+      get() {
+        return this.isShow // isShow会动态变化，依赖监视属性
+      },
+      // 写计算属性
+      set() {
+        // 滑动刷新的前提：只有一个列表，动态的加减数据（不能和多列表，创建滑动混淆）
+        if (this.isShow) {
+          this.$nextTick(() => {
+            // 页面更新后创建滑动，只需要创建单例滑动（防止生成多个滑动列表）
+            if (!this.scroll) {
+              this.scroll = new BScroll('.list-content', { click: true })
+            } else {
+              this.scroll.refresh() // 让滑动条刷新一下，重新统计内容的高度（判断是否需要滑动条）
+            }
+          })
+        }
+      },
     },
   },
-
+  watch: {
+    // 动态改变isShow
+    totalCount() {
+      if (this.totalCount === 0) this.isShow = false
+    },
+  },
   methods: {
     toggleShow() {
       // 只有当总数量大于0时切换
       if (this.totalCount > 0) {
         this.isShow = !this.isShow
       }
+      // 切换后有购物车列表，此时修改计算属性，目的就是创建滑动 或者 refresh滑动
+      this.listShow = false
     },
-
     clearCart() {
-      MessageBox.confirm('确定清空购物车吗?').then(
+      MessageBox.confirm('确定清空购物车吗？').then(
         (action) => {
           this.$store.dispatch('clearCart')
+          console.log('确定清空购物车', action)
         },
-        () => {}
+        () => {
+          console.log('取消清空购物车')
+        }
       )
     },
-  },
-  components: {
-    CartControl,
   },
 }
 </script>
@@ -168,7 +175,7 @@ export default {
           &.highlight {
             background: #02a774;
           }
-          .icon-accountbook-fill {
+          .icon-shopping_cart {
             line-height: 44px;
             font-size: 24px;
             color: #80858a;
@@ -257,14 +264,14 @@ export default {
     top: 0;
     z-index: -1;
     width: 100%;
-    transform: translateY(-100%);
+    transform: translateY(-100%); // 动画：向上移动100%自身高度
     &.move-enter-active,
     &.move-leave-active {
       transition: transform 0.3s;
     }
     &.move-enter,
     &.move-leave-to {
-      transform: translateY(0);
+      transform: translateY(0); // 起始高度就是0
     }
     .list-header {
       height: 40px;
